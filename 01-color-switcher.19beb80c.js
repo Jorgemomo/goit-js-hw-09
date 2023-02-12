78758b7a78971a6cfb8135417d89ec594d79d443
Jorgemomo <108906263+Jorgemomo@users.noreply.github.com>
2023-02-12 16:30:20 +0000

    Deploying to gh-pages from @ Jorgemomo/goit-js-hw-09@6dc9613240ec490ff732928a3eae24f46b2e49df 🚀

---
+++ 01-color-switcher.19beb80c.js
@@ -0,0 +1,2 @@
+const t={startButton:document.querySelector("button[data-start]"),stopButton:document.querySelector("button[data-stop]"),bgBody:document.querySelector("body")};let o=null;function n(){t.bgBody.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.startButton.addEventListener("click",(function(){t.startButton.disabled=!0,t.stopButton.disabled=!1,o=setInterval(n,1e3)})),t.stopButton.addEventListener("click",(function(){t.startButton.disabled=!1,t.stopButton.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.19beb80c.js.map
